bd9dcc0b79036938e534d90a2ad4a79b68defddc
Jessica <jessicaesimpson91@gmail.com>
2018-04-03 16:38:23 -0500

    math module

---
+++ node/mathModule/app.js
@@ -0,0 +1,9 @@
+var math_module = require('./mathLib')();
+
+console.log(math_module.add(11, 12))
+
+console.log(math_module.multiply(2, 5))
+
+console.log(math_module.random(1, 100))
+
+console.log(math_module.square(9))
